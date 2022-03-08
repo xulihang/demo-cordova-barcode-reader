49d9b301902649529763243ed35b6139bd7e0580
xulihang <xulihanghai@163.com>
2022-03-08 17:47:29 +0800

    add live scan

--- www/js/index.js
+++ www/js/index.js
@@ -1,25 +1,10 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
+let scanningInterval;
+let cameraOn = false;
+let decoding = false;
+let desiredTorchStatus = "on";
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
@@ -29,6 +14,95 @@ function onDeviceReady() {
     for (let i=0;i<buttons.length;i++) {
         buttons[i].disabled = "";
     }
+}
+
+function destroy(){
+    cordova.plugins.DBR.destroy();
+}
+
+function startScan(){
+    document.body.style.backgroundColor = "transparent";
+    addSVGOverlay(1280,720);
+    cordova.plugins.DBR.startScanning("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", onScanned);
+}
+
+function stopScan(){
+    document.body.style.backgroundColor = "white";
+    cordova.plugins.DBR.stopScanning();
+}
+
+function pauseScan(){
+    cordova.plugins.DBR.pauseScanning();
+}
+function resumeScan(){
+    cordova.plugins.DBR.resumeScanning();
+}
+
+function onScanned(scanResult){
+    console.log(scanResult);
+    showOverlayOnSVG(scanResult);
+}
+
+function switchTorch(){
+    cordova.plugins.DBR.switchTorch(desiredTorchStatus);
+    if (desiredTorchStatus == "on") {
+        desiredTorchStatus = "off";
+    } else {
+        desiredTorchStatus = "on";
+    }
+}
+
+function addSVGOverlay(width, height){
+    var overlayContainer = document.getElementsByClassName("overlay")[0];
+    if (overlayContainer.getElementsByTagName("svg").length>0) {
+        return;
+    }
+    //var video = document.getElementsByClassName("dbrScanner-video")[0];
+    var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
+    svg.style.top = 0;
+    svg.style.left = 0;
+    svg.style.position = "absolute";
+    svg.style.width = "100%";
+    svg.style.height = "100%";
+    svg.style.zIndex = 0;
+    svg.setAttribute("viewBox","0 0 "+width+" "+height);
+    overlayContainer.appendChild(svg);
+}
+
+function showOverlayOnSVG(scanResult){
+    var overlayContainer = document.getElementsByClassName("overlay")[0];
+    if (overlayContainer.getElementsByTagName("svg").length>0) {
+        var svg = overlayContainer.getElementsByTagName("svg")[0];
+        svg.innerHTML=""
+        var results = scanResult.results;
+        svg.setAttribute("viewBox","0 0 "+scanResult.frameWidth+" "+scanResult.frameHeight);
+        for(let i = 0; i < results.length; i++){
+            var result = results[i];
+            var points = getPointsData(result);
+            var a = document.createElementNS("http://www.w3.org/2000/svg","a");
+            var polygon = document.createElementNS("http://www.w3.org/2000/svg","polygon");
+            polygon.setAttribute("points",points);
+            polygon.setAttribute("class","barcode-polygon");
+
+            var text = document.createElementNS("http://www.w3.org/2000/svg","text");
+            text.setAttribute("x",result.x1);
+            text.setAttribute("y",result.y1);
+            text.setAttribute("fill","red");
+            text.textContent = result.barcodeText;
+            text.style.fontSize = 20;
+            a.append(polygon);
+            a.append(text);
+            svg.append(a);
+        }
+    }
+}
+
+function getPointsData(lr){
+    var pointsData = lr.x1+","+lr.y1 + " ";
+    pointsData = pointsData+ lr.x2+","+lr.y2 + " ";
+    pointsData = pointsData+ lr.x3+","+lr.y3 + " ";
+    pointsData = pointsData+ lr.x4+","+lr.y4;
+    return pointsData;
 }
 
 function takeAndDecode(){
@@ -47,17 +121,17 @@ function onInit() {
 
 function onDecoded(results) {
     let resultHTML = "";
+    decoding = false;
     if (results.length>0) {
         for (let i=0;i<results.length;i++) {
             let result = results[i];
             resultHTML = resultHTML + "<li>"
             resultHTML = resultHTML + result.barcodeFormat + ": " + result.barcodeText;
             resultHTML = resultHTML + "</li>"
-        }  
+        }
     }else{
         resultHTML = "No barcodes found.";
     }
-    
     document.getElementById("results").innerHTML = resultHTML;
 }
 
